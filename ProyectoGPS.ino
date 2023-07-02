a99212bc1dab7976d5554968d3730ceb5675d317
zlakcswair <87821918+zlakcswair@users.noreply.github.com>
2023-07-02 15:47:20 -0500

    Update ProyectoGPS.ino

--- ProyectoGPS.ino
+++ ProyectoGPS.ino
@@ -1,51 +1,45 @@
 //FirebaseESP8266.h must be included before ESP8266WiFi.h
-#include <FirebaseESP8266.h>  //https://github.com/mobizt/Firebase-ESP8266
-#include <ESP8266WiFi.h>
-#include <SoftwareSerial.h>
-#include <TinyGPS++.h> //https://github.com/mikalhart/TinyGPSPlus
+#include <FirebaseESP8266.h>  // Permite a tu ESP8266 comunicarse con Firebase
+#include <ESP8266WiFi.h>      // Proporciona métodos para conectarte a una red WiFi
+#include <SoftwareSerial.h>   // Permite la comunicación serie en otros pines digitales
+#include <TinyGPS++.h>        // Biblioteca GPS personalizada para Arduino
 
-//Install ArduinoJson Library
-//Note: The latest JSON library might not work with the code. 
-//So you may need to downgrade the library to version v5.13.5
-//-----------------------------------------------------------------------------------
-
-//-----------------------------------------------------------------------------------
-#define FIREBASE_HOST "proyecto-gps-d4cb3-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "0bloeGMvsckqPhjzWebl0ZDkthk7oejwiFNBsuM35BrmKW"
-#define WIFI_SSID "Net"
-#define WIFI_PASSWORD "ldub3365"
+//Definición de constantes de conexión para Firebase y WiFi
+#define FIREBASE_HOST "proyecto-gps-d4cb3-default-rtdb.firebaseio.com" // Dirección de la base de datos Firebase
+#define FIREBASE_AUTH "0bloeGMvsckqPhjzWebl0ZDkthk7oejwiFNBsuM35BrmKW" // Clave de autenticación de Firebase
+#define WIFI_SSID "Net" // Nombre de la red WiFi a la que se conectará
+#define WIFI_PASSWORD "ldub3365" // Contraseña de la red WiFi a la que se conectará
 //-----------------------------------------------------------------------------------
 
 
 //-----------------------------------------------------------------------------------
 //Define FirebaseESP8266 data object
-FirebaseData firebaseData;
-
-FirebaseJson json;
+FirebaseData firebaseData; // Almacenará datos y resultados devueltos por el servidor Firebase
+FirebaseJson json; // Para manipular datos en formato JSON
 //-----------------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------------
-//GPS Module RX pin to NodeMCU D1
-//GPS Module TX pin to NodeMCU D2
-const int RXPin = 4, TXPin = 5;
-SoftwareSerial neo6m(RXPin, TXPin);
-TinyGPSPlus gps;
+// Definir los pines de conexión para el módulo GPS
+const int RXPin = 4, TXPin = 5; // RX y TX para la comunicación con el módulo GPS
+SoftwareSerial neo6m(RXPin, TXPin); // SoftwareSerial para el módulo GPS
+TinyGPSPlus gps; // Objeto para decodificar los datos del GPS
 //-----------------------------------------------------------------------------------
 
 
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//Configuración inicial del sistema
 void setup()
 {
 
-  Serial.begin(115200);
+  Serial.begin(115200); // Inicia la comunicación serie a 115200 baudios
 
-  neo6m.begin(9600);
+  neo6m.begin(9600);  // Inicia la comunicación serie con el módulo GPS a 9600 baudios
   
-  wifiConnect();
+  wifiConnect();  // Llama a la función para conectar al WiFi
 
   Serial.println("Connecting Firebase.....");
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.reconnectWiFi(true);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); // Inicia la conexión con Firebase
+  Firebase.reconnectWiFi(true); // Se asegura de mantener la conexión WiFi
   Serial.println("Firebase OK.");
 
 }
@@ -53,14 +47,16 @@ void setup()
 
 
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// Bucle principal de ejecución
 void loop() {
   
-  smartdelay_gps(1000);
+  smartdelay_gps(1000); // Se esperan 1000 milisegundos y se aprovecha inteligentemente ese tiempo para llamar a la función que lee y procesa los datos del GPS
 
-  if(gps.location.isValid()) 
+
+  if(gps.location.isValid())  // Si los datos del GPS son válidos
   {
-    float latitude = gps.location.lat();
-    float longitude = gps.location.lng();
+    float latitude = gps.location.lat(); // Obtiene la latitud
+    float longitude = gps.location.lng(); // Obtiene la longitud
     
     //-------------------------------------------------------------
     //Send to Serial Monitor for Debugging
@@ -71,12 +67,12 @@ void loop() {
     //-------------------------------------------------------------
     
     //-------------------------------------------------------------
-    if(Firebase.setFloat(firebaseData, "/GPS/f_latitude", latitude))
+    if(Firebase.setFloat(firebaseData, "/GPS/f_latitude", latitude)) // Envía la latitud a Firebase como un valor flotante
       {print_ok();}
     else
       {print_fail();}
     //-------------------------------------------------------------
-    if(Firebase.setFloat(firebaseData, "/GPS/f_longitude", longitude))
+    if(Firebase.setFloat(firebaseData, "/GPS/f_longitude", longitude)) // Envía la longitud a Firebase como un valor flotante
       {print_ok();}
     else
       {print_fail();}
@@ -84,39 +80,41 @@ void loop() {
   }
   else
   {
-    Serial.println("No valid GPS data found.");
+    Serial.println("No valid GPS data found."); // Si los datos del GPS no son válidos, imprime un mensaje de error
   }
   
-  delay(5000);
+  delay(5000); // Espera 5 segundos antes de la próxima iteración
 }
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 
 
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-static void smartdelay_gps(unsigned long ms)
+//Función para procesar los datos del GPS
+static void smartdelay_gps(unsigned long ms) // Recibe como argumento el número de ms a esperar
 {
-  unsigned long start = millis();
+  unsigned long start = millis(); // Asigna el tiempo actual en milisegundos desde que el programa de Arduino comenzó a ejecutarse.
   do 
   {
-    while (neo6m.available())
-      gps.encode(neo6m.read());
-  } while (millis() - start < ms);
+    while (neo6m.available()) // Este bucle se ejecutará mientras haya datos disponibles para leer desde el módulo GPS.
+      gps.encode(neo6m.read()); // Si hay datos disponibles para leer desde el módulo GPS, lee los datos y los pasa a la función "encode()" de la biblioteca TinyGPS++. Esta función procesa los datos del GPS.
+  } while (millis() - start < ms); // Este bucle continuará ejecutándose hasta que la diferencia entre el tiempo actual y el valor almacenado en "start" sea mayor o igual que "ms".
 }
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 
 
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//Función para conectar al WiFi
 void wifiConnect()
 {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // Se inicia el intento de conexión a la red Wi-Fi
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
+  while (WiFi.status() != WL_CONNECTED) // Mientras no haya conexión se imprimen puntitos.
   {
     Serial.print(".");
     delay(300);
   }
   Serial.println();
-  Serial.print("Connected with IP: ");
+  Serial.print("Connected with IP: "); // Cuando se logra la conexión se muestra la IP.
   Serial.println(WiFi.localIP());
   Serial.println();
 }
@@ -124,6 +122,8 @@ void wifiConnect()
 
 
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+//Funciones para imprimir mensajes de éxito o error
 void print_ok()
 {
     Serial.println("------------------------------------");
@@ -148,6 +148,7 @@ void print_fail()
 }
 //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 
+//Función para reconectar a Firebase si la conexión se pierde
 void firebaseReconnect()
 {
   Serial.println("Trying to reconnect");
